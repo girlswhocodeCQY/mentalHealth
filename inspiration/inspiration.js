bac7ee2847919a2698ec9d9e56b9bc1e2a11cfc2
thinleywangmo <34357775+thinleywangmo@users.noreply.github.com>
2018-03-22 20:44:21 -0400

    Update inspiration.js

--- inspiration/inspiration.js
+++ inspiration/inspiration.js
@@ -2,7 +2,27 @@ console.log('the inspiration page has loaded!');
 
 // TODO
 // put more interesting quotes in here! (use the ones from the document)
-var quotes = [ "don't worry", "be happy", "breathe :)"];
+var quotesList = [ "Nothing is impossible, the word itself says Im possible!",
+"Life is not about waiting for the storm to pass, its about learning to dance in the rain.",
+"breathe :)", "Don't wait for the right opportunity, create it.",
+"Work hard in silence, let your success be the noise.",
+"Tough times never last, but tough people do.",
+"You’re Allowed To Scream, You’re Allowed To Cry, But Do Not Give Up.",
+"If You’re Lucky Enough To Be Different, Don’t Ever Change.",
+"Wake Up With Determination. Go To Bed With Satisfaction",
+"Life is 10% what happens to me and 90% of how I react to it.",
+"Difficult roads often lead to beautiful destinations.",
+"It's a bad day, not a bad life.",
+"If you can dream it, you can do it.",
+"Whenever You Find Yourself Doubting How Far You Can Go, Just Remember How Far You Have Come. Remember Everything You Have Faced, All The Battles You Have Won, And All The Fears You Have Overcome.",
+"Promise yourself you'll always remember: you're braver than you believe, stronger than you seem, and smarter than you think.",
+"Don't let others define you. You define yourself.", "A little Consideration, a little Thought for Others, makes all the difference.",
+"You can't stay in your corner of the forest waiting for others to come to you. You have to go to them sometimes.",
+"It never hurts to keep looking for sunshine.", "The boat only sinks if you let it.",
+"Don't let the behavior of others destroy your inner peace.",
+"If someone is strong enough to brung you down, show them you are strong enough to get up.",
+"Don't let anyone's ignorance, hate, drama, or negativity stop you from being the best person you can be."];
+
 
 window.onload = function() {
 
@@ -10,17 +30,20 @@ window.onload = function() {
   var quotesDiv = document.getElementById('quotes');
 
   // change the text inside of the 'quotes' div in the HTML to say "new text"
-  quotesDiv.innerHTML = "new text";
+  //quotesDiv.innerHTML =   quotesList[0];
 
+  timer();
+  var index = 0;
 
-  // TODO
-  // we want to change the innerHTML of the quotesDiv every 30 seconds
-  // the first time, we want to use the first element from the quotes list
-  // the second time, we want to use the second element from the quotes list
-  // the third time, we want to use the third element from the quotes list
-
-  // how can we write code that
-  // 1. does something every 30 seconds
-  // 2. chooses a new quote from the quotes list every time
+  function timer() {
+    setTimeout( function() {
+      quotesDiv.innerHTML = quotesList[index];
+      index++;
+      if (index === quotesList.Length) {
+        index = 0; 
+      }
+      timer(quotesList[index])
+    }, 5000);
+  }
 
 }
